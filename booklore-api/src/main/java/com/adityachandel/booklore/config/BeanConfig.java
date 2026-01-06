a00b285a900b17d8a3707dd5577452a95b114547
farfromrefuge <dev@akylas.fr>
2026-01-06 21:29:20 +0100

    chore: trying to improve requests time

--- booklore-api/src/main/java/com/adityachandel/booklore/config/BeanConfig.java
+++ booklore-api/src/main/java/com/adityachandel/booklore/config/BeanConfig.java
@@ -1,10 +1,16 @@
 package com.adityachandel.booklore.config;
 
 import jakarta.annotation.PostConstruct;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.config.RequestConfig;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
+import org.apache.hc.core5.util.Timeout;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.socket.config.WebSocketMessageBrokerStats;
 
@@ -18,12 +24,31 @@ public class BeanConfig {
 
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        return builder.connectTimeout(Duration.ofSeconds(10)).readTimeout(Duration.ofSeconds(15))
+        PoolingHttpClientConnectionManager connManager = new PoolingHttpClientConnectionManager();
+        connManager.setMaxTotal(200);
+        connManager.setDefaultMaxPerRoute(50);
+
+        RequestConfig requestConfig = RequestConfig.custom()
+                .setConnectTimeout(Timeout.ofMilliseconds(Duration.ofSeconds(10).toMillis()))
+                .setResponseTimeout(Timeout.ofMilliseconds(Duration.ofSeconds(15).toMillis()))
+                .setConnectionRequestTimeout(Timeout.ofMilliseconds(Duration.ofSeconds(10).toMillis()))
                 .build();
+
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setConnectionManager(connManager)
+                .setDefaultRequestConfig(requestConfig)
+                .build();
+
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient);
+        factory.setConnectTimeout((int) Duration.ofSeconds(10).toMillis());
+        factory.setReadTimeout((int) Duration.ofSeconds(15).toMillis());
+        factory.setConnectionRequestTimeout((int) Duration.ofSeconds(10).toMillis());
+
+        return builder.requestFactory(() -> factory).build();
     }
 
     @PostConstruct
     public void init() {
-        webSocketMessageBrokerStats.setLoggingPeriod(30 * 24 * 60 * 60 * 1000L); // 30 days
+        webSocketMessageBrokerStats.setLoggingPeriod(30L * 24 * 60 * 60 * 1000); // 30 days
     }
 }
